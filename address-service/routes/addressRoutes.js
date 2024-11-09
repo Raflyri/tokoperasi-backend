1bed9e3dcd5296493fb9bcce034be915d858cac1
rbtech <mail.rbtech@gmail.com>
2024-11-10 01:39:51 +0700

    update bugs

--- address-service/routes/addressRoutes.js
+++ address-service/routes/addressRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const addressController = require('../controllers/addressController');
 const authenticate = require('../middleware/authenticate');
 
@@ -18,24 +17,5 @@ router.put('/:id', authenticate, addressController.updateAddress);
 
 // Delete an address
 router.delete('/:id', authenticate, addressController.deleteAddress);
-=======
-const userAddressController = require('../controllers/userAddressController');
-const authenticate = require('../middleware/authenticate'); // Pastikan middleware autentikasi diimpor
-
-// Get all addresses
-router.get('/', authenticate, userAddressController.getAddresses);
-
-// Get address by ID
-router.get('/:id', authenticate, userAddressController.getAddressById);
-
-// Create a new address
-router.post('/', authenticate, userAddressController.addAddress);
-
-// Update an address
-router.put('/:id', authenticate, userAddressController.updateAddress);
-
-// Delete an address
-router.delete('/:id', authenticate, userAddressController.deleteAddress);
->>>>>>> 95192612bce36d7f78911807e3fb1e0fb7576a06
 
 module.exports = router;
